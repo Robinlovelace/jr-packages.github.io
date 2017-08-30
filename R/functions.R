f34974ce5caa448dc6091f057e04cc21933d7477
Colin Gillespie <csgillespie@gmail.com>
2017-08-30 23:03:01 +0100

    Generate standard R and Rstudio header

--- R/functions.R
+++ R/functions.R
@@ -3,3 +3,10 @@ current_r_version = function() {
   r = strsplit(r, split = "[.]")[[1]]
   paste(r, collapse =".")
 }
+
+r_studio_header = function() {
+"### R and RStudio
+
+Please make you sure you have the latest versions of R and RStudio (see the [introduction](../introduction) page
+                                                                    for details)."
+}
